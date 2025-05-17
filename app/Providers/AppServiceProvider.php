4915c0ebcaa4cb9dcac31d1b9022a83a2ad2ed4e
Devender Kumar <lucdev93@gmail.com>
2025-05-17 09:15:44 +0530

    mixed content

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace App\Providers;
-
+use Illuminate\Routing\UrlGenerator;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -17,9 +17,12 @@ class AppServiceProvider extends ServiceProvider
     /**
      * Bootstrap any application services.
      */
-    public function boot(): void
+    public function boot(UrlGenerator $url)
     {
-        
+        if(env('APP_ENV') !== 'local')
+        {
+            $url->forceSchema('https');
+        } 
           
         
     }
