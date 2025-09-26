6353178933372b7f18d9a36e61ef3c03278d0413
DesignerNik05 <wd.nikitadhiman@gmail.com>
2025-09-26 19:21:25 +0530

    make signature large

--- resources/views/pdf/student_report.blade.php
+++ resources/views/pdf/student_report.blade.php
@@ -534,7 +534,7 @@ p {
                 </div>
                 <div class="column" style="padding-left:5%;">
                     <div class="signatureImg" style="text-align: center;">
-                        <img src="{{ public_path('script-sign.png') }}" style="width: 120px;">
+                        <img src="{{ public_path('script-sign.png') }}" style="width: 200px; margin-top: --60px;">
                         <p class="signaturetxt" style="border-top: 2px solid #000000;">Registrar</p>
                     </div>
                 </div>
