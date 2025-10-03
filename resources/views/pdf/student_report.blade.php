2a84e5385664cdc3e1ae6fa92940b75b9f6f30e8
DesignerNik05 <wd.nikitadhiman@gmail.com>
2025-10-04 00:35:27 +0530

    fixed header

--- resources/views/pdf/student_report.blade.php
+++ resources/views/pdf/student_report.blade.php
@@ -111,19 +111,19 @@ p {
             <table width="100%" style="border-top: 2px solid #000000; padding: 10px 0;">
                 <tbody>
                     <tr>
-                        <td><b style="width: 150px; display: inline-block;">Name:</b> {{ $student->name }}</td>
-                        <td><b style="width: 150px; display: inline-block;">Date of Birth:</b> {{ $student->dob->format('m/d/Y') }}</td>
-                        <td><b style="width: 150px; display: inline-block;">Guardian:</b> {{ $student->guardian_name ?? 'N/A' }}</td>
+                        <td><b>Name:</b> {{ $student->name }}</td>
+                        <td><b>Date of Birth:</b> {{ $student->dob->format('m/d/Y') }}</td>
+                        <td><b>Guardian:</b> {{ $student->guardian_name ?? 'N/A' }}</td>
 			</tr>
 			<tr>
-                        <td><b style="width: 150px; display: inline-block;">Address:</b> {{ $student->address ?? 'N/A' }}</td>
-                        <td><b style="width: 150px; display: inline-block;">Gender:</b> {{ $student->gender ?? 'N/A' }}</td>
-                        <td><b style="width: 150px; display: inline-block;">Counselor:</b> {{ $student->counselor ?? 'N/A' }}</td>
+                        <td><b  >Address:</b> {{ $student->address ?? 'N/A' }}</td>
+                        <td><b>Gender:</b> {{ $student->gender ?? 'N/A' }}</td>
+                        <td><b>Counselor:</b> {{ $student->counselor ?? 'N/A' }}</td>
                         <!-- Debug: Student counselor value: {{ $student->counselor }} -->
 			</tr>
                     <tr>
                         <td></td>
-                        <td><b style="width: 150px; display: inline-block;">Date of Graduation:</b> {{ $student->graduation_date ? $student->graduation_date->format('m/d/Y') : 'N/A' }}</td>
+                        <td><b>Date of Graduation:</b> {{ $student->graduation_date ? $student->graduation_date->format('m/d/Y') : 'N/A' }}</td>
                         <td></td>
 			</tr>
 		</tbody>
