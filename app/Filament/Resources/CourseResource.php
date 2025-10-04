3bd59e6a7bbcb30c55a080c048c1c5ece43926f9
webciphergenius <lucdev93@gmail.com>
2025-10-04 16:12:55 +0530

    course bulk delete

--- app/Filament/Resources/CourseResource.php
+++ app/Filament/Resources/CourseResource.php
@@ -48,6 +48,11 @@ class CourseResource extends Resource
         ->actions([
             Tables\Actions\EditAction::make(),
             Tables\Actions\DeleteAction::make(),
+        ])
+        ->bulkActions([
+            Tables\Actions\BulkActionGroup::make([
+                Tables\Actions\DeleteBulkAction::make(),
+            ]),
         ]);
     }
 
